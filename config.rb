aea8fe055ae4328cb97874f7520dc7bcc46b4c22
John Reading <john@resonancemultimedia.com>
2012-03-19 14:36:19 -0400

    try this again

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/HelloWurld/"
+http_images_path = "/HelloWurld/images"
+http_fonts_path = "/HelloWurld/fonts"
+css_dir = "public/HelloWurld/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
